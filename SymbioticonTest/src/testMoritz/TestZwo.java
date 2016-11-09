78e05864e2e228349610ef5e7a48b121e3d48253
Moritz <moritzschlueter@gmx.net>
2016-11-09 19:36:58 +0100

    Test für Commit ohne Staging

--- SymbioticonTest/src/testMoritz/TestZwo.java
+++ SymbioticonTest/src/testMoritz/TestZwo.java
@@ -5,6 +5,7 @@ public class TestZwo {
 	public static void main(String[] args) {
 		System.out.println("Was tue ich hier?");
 		System.out.println("keine Ahnung");
+		System.out.println("Wann kann ich gehen?");
 	}
 
 }
