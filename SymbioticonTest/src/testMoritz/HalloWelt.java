55d5428df70cf31c1cdb7e50ae7d2d2258f7ab70
Moritz <Moritz@MoritzLaptop>
2016-11-09 19:29:51 +0100

    noch eine Zeile hinzugefügt

--- SymbioticonTest/src/testMoritz/HalloWelt.java
+++ SymbioticonTest/src/testMoritz/HalloWelt.java
@@ -5,6 +5,7 @@ public class HalloWelt {
 	public static void main(String[] args) {
 		System.out.println("Hier könnte Ihre Werbung stehen.");
 		System.out.println("noch mehr Text");
+		System.out.println("Wer da liest ist doof");
 	}
 
 }
